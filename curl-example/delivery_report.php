dcc972a46e097295460b661505a54c0191493741
kazashim kuzasuwat <kazashimkuzasuwat@gmail.com>
2021-01-05 12:03:22 +0200

    Update delivery_report.php

--- curl-example/delivery_report.php
+++ curl-example/delivery_report.php
@@ -1,6 +1,6 @@
 <?php
 
-// Step 1: set your API_KEY from https://mywebhost.com/sms-api/info
+// Step 1: set your API_KEY from https://sms.cynojine.com/sms-api/info
 
 $api_key = 'xxxxxxxxxxxxxxxxxxxxxxxx';
 
@@ -18,3 +18,21 @@ $sms_body = array(
 $send_data = http_build_query($sms_body);
 
 $gateway_url = $url . "?" . $send_data;
+
+try {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $gateway_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPGET, 1);
+    $output = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $output = curl_error($ch);
+    }
+    curl_close($ch);
+
+    var_dump($output);
+
+}catch (Exception $exception){
+    echo $exception->getMessage();
+}
